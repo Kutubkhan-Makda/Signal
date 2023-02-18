7a810b5dc09c1b685a76592da5dda87923d5c271
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-18 21:58:13 +0530

    day-1

--- Screens/LoginScreen.jsx
+++ Screens/LoginScreen.jsx
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React, { useState } from 'react'
 import { Button, Image, Input } from '@rneui/themed';
+import { KeyboardAvoidingView } from 'react-native';
 
 const LoginScreen = () => {
     const [email, setEmail] = useState('');
@@ -11,7 +12,7 @@ const LoginScreen = () => {
     }
 
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 0} style={styles.container}>
       <Image source={require('../assets/Logo_Signal.png')} style={{width:200,height:200}}/>
       <View style={styles.inputContainer}>
         <Input placeholder='Email' autoFocus type='email' value={email} onChangeText={text => setEmail(text)}/>
@@ -19,7 +20,8 @@ const LoginScreen = () => {
       </View>
       <Button containerStyle={styles.button} title='Login' onPress={signIn}/>
       <Button containerStyle={styles.button} type='outline' title='Register'/>
-    </View>
+      <View style={{height:100}}></View>
+    </KeyboardAvoidingView>
   )
 }
 
@@ -27,7 +29,10 @@ export default LoginScreen
 
 const styles = StyleSheet.create({
     container:{
-
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center',
+        padding:10,
     },
     inputContainer:{
 
