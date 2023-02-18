9abee8de24c4dca96e27d32be2b834a0781da02c
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-18 15:24:40 +0530

    day-1

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD-qSxDri-SJvztsWL2Aq4a1MNgliNsG30",
+  authDomain: "signal-52248.firebaseapp.com",
+  projectId: "signal-52248",
+  storageBucket: "signal-52248.appspot.com",
+  messagingSenderId: "662515208935",
+  appId: "1:662515208935:web:aa5ab434dec9ddc64b4842"
+};
