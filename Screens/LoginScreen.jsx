4b03f4548caa9f4ae9acdec53a6387d88078c13c
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-18 20:22:09 +0530

    day-1

--- Screens/LoginScreen.jsx
+++ Screens/LoginScreen.jsx
@@ -1,15 +1,22 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { Button, Image } from '@rneui/themed';
+import { Button, Image, Input } from '@rneui/themed';
 
 const LoginScreen = () => {
   return (
     <View>
       <Image source={require('../assets/Logo_Signal.png')} style={{width:200,height:200}}/>
+      <View style={styles.inputContainer}>
+        <Input placeholder=''/>
+      </View>
     </View>
   )
 }
 
 export default LoginScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    inputContainer:{
+
+    },
+})
