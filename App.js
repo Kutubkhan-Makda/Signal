35e0e3cc1208a1cc62e274e174397dd69b3fe7ba
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-18 19:38:39 +0530

    day-1

--- App.js
+++ App.js
@@ -2,20 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './Screens/LoginScreen';
 
 const Stack = createNativeStackNavigator();
+const globalScreenOptions = {
+  headerStyle:{backgroundColor:'#2C6BED',textAlign:'center'},
+  headerTitleStyle:{color:'white'},
+  headerTintColor:'white',
+};
 
 export default function App() {
   return (
     <SafeAreaView style={styles.AndroidSafeArea}>
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator screenOptions={globalScreenOptions}>
           <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Login" component={LoginScreen} />
         </Stack.Navigator>
-        <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
-          <StatusBar style="auto" />
-        </View>
       </NavigationContainer>
     </SafeAreaView>
   );
