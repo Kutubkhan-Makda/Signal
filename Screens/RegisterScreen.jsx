8258f00b23010399a3ad9592ebe94b504ffcc892
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-22 00:00:18 +0530

    day-3

--- Screens/RegisterScreen.jsx
+++ Screens/RegisterScreen.jsx
@@ -21,7 +21,7 @@ const RegisterScreen = () => {
     const register = ()=>{
         auth.createUserWithEmailAndPassword(email,password)
         .then(authUser => {
-            authUser.user.update({displayName:name,photoURL:imageUrl || "https://t4.ftcdn.net/jpg/05/11/55/91/360_F_511559113_UTxNAE1EP40z1qZ8hIzGNrB0LwqwjruK.jpg"})
+            authUser.user.updateProfile({displayName:name,photoURL:imageUrl || "https://t4.ftcdn.net/jpg/05/11/55/91/360_F_511559113_UTxNAE1EP40z1qZ8hIzGNrB0LwqwjruK.jpg"})
         }).catch((error)=>alert(error.message));
     }
 
