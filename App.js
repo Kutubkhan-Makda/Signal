40829834ed227ab643e9bee45f78b51124c47825
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-22 19:53:43 +0530

    day-4

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './Screens/LoginScreen';
 import RegisterScreen from './Screens/RegisterScreen';
 import HomeScreen from './Screens/HomeScreen';
-import { Avatar } from '@rneui/themed';
-import {auth,db} from './firebase'
 
 const Stack = createNativeStackNavigator();
 const globalScreenOptions = {
@@ -17,12 +15,6 @@ const globalScreenOptions = {
 };
 
 export default function App() {
-  const navigation = useNavigation();
-
-  const signOut = ()=>{
-    auth.signOut().then(()=>{navigation.navigate('Login')})
-  }
-
   return (
     <SafeAreaView style={styles.AndroidSafeArea}>
       <StatusBar style='light'/>
@@ -30,20 +22,7 @@ export default function App() {
         <Stack.Navigator screenOptions={globalScreenOptions}>
           <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="Register" component={RegisterScreen} />
-          <Stack.Screen name="Home" component={HomeScreen} options={{title:'Signal',
-          headerStyle:{backgroundColor:'#fff'},
-          statusBarStyle:'dark',
-          headerTitleStyle:{color:'black'},
-          headerLeft:()=>(
-            <View style={{marginLeft:7}}>
-              <TouchableOpacity onPress={signOut} activeOpacity={0.5}>
-                <Avatar rounded source={{uri:auth?.currentUser?.photoURL}}/>
-              </TouchableOpacity>
-            </View>
-          ),
-          headerRight:()=>(
-            
-          ),}}/>
+          <Stack.Screen name="Home" component={HomeScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaView>
--- Screens/HomeScreen.jsx
+++ Screens/HomeScreen.jsx
@@ -1,11 +1,44 @@
 import { SafeAreaView, ScrollView, StyleSheet } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import CustomListItem from '../Components/CustomListItem'
+import { useNavigation } from '@react-navigation/native'
+import { Avatar, Icon } from '@rneui/themed'
+import { auth,db } from '../firebase'
+import { View } from 'react-native'
+import { TouchableOpacity } from 'react-native'
+
 
 const HomeScreen = () => {
-    useLayoutEffect(() => {
-        title:''
-    }, [])
+  const navigation = useNavigation();
+
+  const signOut = ()=>{
+    auth.signOut().then(()=>{navigation.navigate('Login')})
+  }
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+    title:'Signal',
+    headerStyle:{backgroundColor:'#fff'},
+    statusBarStyle:'dark',
+    headerTitleStyle:{color:'black'},
+    headerLeft:()=>(
+      <View style={{marginLeft:7}}>
+        <TouchableOpacity onPress={signOut} activeOpacity={0.5}>
+          <Avatar rounded source={{uri:auth?.currentUser?.photoURL}}/>
+        </TouchableOpacity>
+      </View>
+    ),
+    headerRight:()=>(
+      <View style={{flexDirection:'row',justifyContent:'space-between',width:80,height:20}}>
+        <TouchableOpacity activeOpacity={0.5}>
+          <Icon type='antdesign' name='camerao' sixe={24} color='black'/>
+        </TouchableOpacity>
+        <TouchableOpacity activeOpacity={0.5}>
+          <Icon type='simplelineicons' name='pencil' sixe={24} color='black'/>
+        </TouchableOpacity>
+      </View>
+    ),})
+  }, [navigation])
 
   return (
     <SafeAreaView>
