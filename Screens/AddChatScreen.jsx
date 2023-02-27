4d16d1f407a8e04bbcdfe696048045c07fa7838a
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-27 20:24:50 +0530

    day-5

--- Screens/AddChatScreen.jsx
+++ Screens/AddChatScreen.jsx
@@ -1,11 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React, { useLayoutEffect } from 'react'
+import React, { useLayoutEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
-import { Icon, Input } from '@rneui/themed';
+import { Button, Icon, Input } from '@rneui/themed';
 
 const AddChatScreen = () => {
     const navigation = useNavigation();
-    const [input, setInput] = useState('');
+    const [input, setInput] = useStatea('');
 
     useLayoutEffect(() => {
       navigation.setOptions({
@@ -17,6 +17,7 @@ const AddChatScreen = () => {
   return (
     <View style={styles.container}>
       <Input placeholder='Enter a chat name' value={input} onChangeText={(text)=>setInput(text)} leftIcon={<Icon name='wechat' type='antdesign' size={34} color='black'/>}/>
+      <Button onPress={createChat} title='Create New Chat'/>
     </View>
   )
 }
