e5d903bd98ddef9a7e379eb5c389d108062017a5
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-03-17 21:52:56 +0530

    day-6

--- Screens/ChatScreen.jsx
+++ Screens/ChatScreen.jsx
@@ -1,10 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native';
+import { Avatar } from '@rneui/themed';
 
-const ChatScreen = () => {
+const ChatScreen = ({route}) => {
+    const navigation = useNavigation();
+
+    useLayoutEffect(()=>{
+        navigation.setOptions({
+            title:'Chat',
+            headerBackTitleVisible:false,
+            headerTitleAlign:'left',
+            headerTitle:()=>(
+                <View style={{flexDirection:'row',alignItems:'center'}}>
+                    <Avatar rounded source={{uri:'https://e7.pngegg.com/pngimages/985/993/png-clipart-company-businessperson-board-of-directors-advanced-orthopaedic-centers-avtar-company-service.png'}}/>
+                    <Text style={{color:'white',marginLeft:10,fontWeight:'700'}}>{route.params.chatName}</Text>
+                </View>
+            ),
+            headerLeft:()=>(
+                <TouchableOpacity></TouchableOpacity>
+            ),
+        })
+    },[navigation])
+
   return (
     <View>
-      <Text>ChatScreen</Text>
+      <Text>{route.params.chatName}</Text>
     </View>
   )
 }
